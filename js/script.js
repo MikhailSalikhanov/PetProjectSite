947c2caab4ede4a922885a1dd88f7b126ac200ca
MikhailSalikhanov <mikhail.salikhanov@gmail.com>
2022-06-08 12:58:49 +0300

    mobile beta 1.0

--- js/script.js
+++ js/script.js
@@ -1,53 +1,51 @@
 'use strict';
-var scroolActive = document.querySelector('.scrool_active');
+var scrollActive = document.querySelector('.scroll_active');
 
-let scroolBefore = document.querySelector('.example_picture_scrool_line_before');
-let scroolAfter = document.querySelector('.example_picture_scrool_line_after');
+let scrollBefore = document.querySelector('.example_picture_scroll_line_before');
+let scrollAfter = document.querySelector('.example_picture_scroll_line_after');
 let cat1 = document.querySelector('.example_picture_cat1');
 let cat2 = document.querySelector('.example_picture_cat2');
 
 function transformToAfter() {
-    scroolAfter.classList.add('scrool_active');
-    scroolBefore.classList.remove('scrool_active');
+    scrollAfter.classList.add('scroll_active');
+    scrollBefore.classList.remove('scroll_active');
     cat1.classList.add('hidden_cat');
     cat1.classList.remove('unhidden_cat');
     cat2.classList.remove('hidden_cat');
     cat2.classList.add('unhidden_cat');
-    // scroolBefore.removeEventListener('mousedown', onMouseDown1);
 };
 
 function transformToBefore () {
-    scroolBefore.classList.add('scrool_active');
-    scroolAfter.classList.remove('scrool_active');
+    scrollBefore.classList.add('scroll_active');
+    scrollAfter.classList.remove('scroll_active');
     cat1.classList.remove('hidden_cat');
     cat1.classList.add('unhidden_cat');
     cat2.classList.remove('unhidden_cat');
     cat2.classList.add('hidden_cat');
-    // scroolAfter.removeEventListener('mousedown', onMouseDown2);
 };
 
-scroolBefore.onclick = function() {
+scrollBefore.onclick = function() {
     transformToBefore ();
 };    
 
-scroolAfter.onclick = function() {
+scrollAfter.onclick = function() {
     transformToAfter ();
 };
 
 
-scroolBefore.addEventListener ('mousedown', onMouseDown1);
-scroolAfter.addEventListener ('mousedown', onMouseDown2);
+scrollBefore.addEventListener ('mousedown', onMouseDown1);
+scrollAfter.addEventListener ('mousedown', onMouseDown2);
 
-scroolBefore.addEventListener ('touchstart', onMouseDown11);
-scroolAfter.addEventListener ('touchstart', onMouseDown22);
+scrollBefore.addEventListener ('touchstart', onMouseDown11);
+scrollAfter.addEventListener ('touchstart', onMouseDown22);
 
 function onMouseDown11 (event){
-    if (scroolBefore.classList.contains('scrool_active')) {
+    if (scrollBefore.classList.contains('scroll_active')) {
         let firstTouch = event.touches[0].clientX;
         document.addEventListener('touchend', onMouseMove);
         function onMouseMove (event){
-            let carrentX = event.changedTouches[event.changedTouches.length-1].pageX;
-            if (firstTouch < carrentX) {
+            let currentX = event.changedTouches[event.changedTouches.length-1].pageX;
+            if (firstTouch < currentX) {
                 transformToAfter();           
                 document.removeEventListener ('touchend', onMouseMove);
             } 
@@ -56,12 +54,12 @@ function onMouseDown11 (event){
 }
 
 function onMouseDown22 (event){
-    if (scroolAfter.classList.contains('scrool_active')) {
+    if (scrollAfter.classList.contains('scroll_active')) {
         let firstTouch = event.touches[0].clientX;
         document.addEventListener('touchend', onMouseMove);
         function onMouseMove (event){
-            let carrentX = event.changedTouches[event.changedTouches.length-1].pageX;
-            if (firstTouch > carrentX) {
+            let currentX = event.changedTouches[event.changedTouches.length-1].pageX;
+            if (firstTouch > currentX) {
                 transformToBefore();           
                 document.removeEventListener ('touchend', onMouseMove);
             } 
@@ -69,28 +67,12 @@ function onMouseDown22 (event){
     }
 }
 
-var src = document.getElementById("source");
-
-src.addEventListener('touchmove', function(e) {
-  // Iterate through the touch points that have moved and log each
-  // of the pageX/Y coordinates. The unit of each coordinate is CSS pixels.
-  var i;
-  for (i=0; i < e.changedTouches.length; i++) {
-    console.log("touchpoint[" + i + "].pageX = " + e.changedTouches[i].pageX);
-    console.log("touchpoint[" + i + "].pageY = " + e.changedTouches[i].pageY);
-  }
-}, false);
-  
-// scroolActive.addEventListener ('mousedown', onMouseDown);
-
 function onMouseDown1 (event){
-    if (scroolBefore.classList.contains('scrool_active')) {
-        // console.log(scroolActive);
-        let carrentX = event.pageX;
-            // console.log(carrentX);
+    if (scrollBefore.classList.contains('scroll_active')) {
+        let currentX = event.pageX;
             document.addEventListener('mousemove', onMouseMove);
             function onMouseMove (event){
-            if (event.pageX > carrentX) {
+            if (event.pageX > currentX) {
                 transformToAfter();           
                 document.removeEventListener ('mousemove', onMouseMove);
             } 
@@ -99,13 +81,11 @@ function onMouseDown1 (event){
 }
 
 function onMouseDown2 (event){
-    if (scroolAfter.classList.contains('scrool_active')) {
-        // console.log(scroolActive);
-        let carrentX = event.pageX;
-        // console.log(carrentX);
+    if (scrollAfter.classList.contains('scroll_active')) {
+        let currentX = event.pageX;
         document.addEventListener('mousemove', onMouseMove);
         function onMouseMove (event){
-            if (event.pageX < carrentX)  {
+            if (event.pageX < currentX)  {
                 transformToBefore();
                 document.removeEventListener ('mousemove', onMouseMove);
             }
@@ -114,10 +94,26 @@ function onMouseDown2 (event){
 }
     
 
-
-
 let cards = document.querySelectorAll('.cards_wrapper');
 for (let one of cards)
     one.onclick = function(){
     one.classList.toggle("cards_wrapper_clicked");
+}
+
+
+let map = document.querySelector('.location_frame');
+let pin = document.querySelector('.location_pin');
+
+map.onmouseover = function(){
+    pin.classList.add('hidden_pin');
+    pin.classList.remove('unhidden_pin');
+}
+
+map.onmousedown = function(){
+    pin.classList.add('none_pin');
+}
+
+map.onmouseout = function(){
+    pin.classList.add('unhidden_pin');
+    pin.classList.remove('hidden_pin');
 }
